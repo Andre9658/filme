1874d79f372698fec3a50287b99c93767fa42681
Andre9658 <andrelimanegocio@gmail.com>
2025-05-07 12:00:10 -0300

    filme-1

--- src/pages/cadastrar/index.jsx
+++ src/pages/cadastrar/index.jsx
@@ -1,36 +1,95 @@
+import instance from "@/api/instance";
 import CustomSelect from "@/Components/CustomSelect";
 import PageWrapper from "@/components/PageWrapper";
 import CustomInput from "@/CustomInput";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function Cadastrar(){
-    return (
+    const [titulo, setTitulo] = useState("");
+    const [diretor, setDiretor] = useState ("");
+    const [ano, setAno] = useState (0);
+    const [genero, setGenero] = useState ("");
+    const [nota, setNota] = useState (0);
+    const [sinopse, setSinopse] = useState ("");
+    const [banner, setBanner] = useState ("");
+
+    async function handleSubmit(event){
+        event.preventDefault();
+        
+        try { 
+            await instance.post("/api/movies", {
+                titulo: titulo,
+                diretor: diretor,
+                ano: ano,
+                genero: genero,
+                nota: nota,
+                sinopse: sinopse,
+                banner: banner
+            })
+
+            toast.success("Filme cadastrado com sucesso!")
+            setTitulo=("");
+            setDiretor=("");
+            setAno=("");
+            setGenero=("");
+            setNota=("");
+            setSinopse=("");
+            setBanner=("");
+            
+        } catch (error) {
+           console.error(error) 
+           toast.error("Erro ao cadastrar filme!")
+        }
+    }
+        
+        
+  return (
         <PageWrapper  showButton={false}>
             <div className="w-full h-full py-[40px] px-[70px] flex flex-col">
                 <h1 className="text-[40px] font-bold text-[#9A86F4]">Adicione um novo filme</h1>
                 <p className="text-[20px] text-gray-500">Preencha o formulário abaixo para adicionar um novo filme à sua coleção.</p>
                 <div className="w-full flex justify-center pt-[40px]">
-                    <form className="w-[50%] h-auto min-h-[200px] bg-[#222222] rounded-2xl border border-[#3a364c] flex flex-col p-6 gap-4">
+                    <form 
+                    onSubmit={handleSubmit}
+                    className="w-[50%] h-auto min-h-[200px] bg-[#222222] rounded-2xl border border-[#3a364c] flex flex-col p-6 gap-4">
                         <CustomInput
                             label="Título"
+                            value={titulo}
                             placeholder="Digite o tÍtulo do filme"
                             type="text"
+                            onChange={
+                                (event) => setTitulo(event.target.value)
+                            }
                         />
                            <CustomInput
                             label="Diretor"
+                            value={diretor}
                             placeholder="Digite o nome do Diretor"
                             type="text"
+                            onChange={
+                                (event) => setDiretor(event.target.value)
+                            }
                         />
                         <div className="w-full flex gap-4">
                             <div className="w-[50%]">
                                 <CustomInput
                                 label="Ano"
+                                value={ano}
                                 placeholder="Digite o ano de lançamento"
                                 type="number"
+                                onChange={
+                                    (event) => setAno(event.target.value)
+                                }
                             />
                             </div>
                             <div className="w-[50%]">
                                 <CustomSelect
+                                onChange={
+                                    (event) => setGenero(event.target.value)
+                                }
                                 label="Genero"
+                                value={genero}
                                 options={[
                                     "Ação",
                                      "Comédia",
@@ -46,24 +105,38 @@ export default function Cadastrar(){
                     <div className="w-[30%] flex gap-2">
                         <div className="w-[70%]">
                         <CustomInput
+                        onChange={
+                            (event) => setNota (event.target.value)
+                        }
                         label="Nota"
+                        value={nota}
                         placeholder="0"
                         type="number"
                         />
                         </div>
                         <p className="flex mt-11">0 / 10</p>
                     </div>
-                    <div className="w-full flex flex-col">
+                    <div 
+                    onChange={
+                        (event) => setSinopse(event.target.value)
+                    }
+                    className="w-full flex flex-col">
                         <label className="text-[17px] font-bold">Sinopse</label>
-                        <textarea className="w-full h-[150px] rounded-lg border focus:border-pink-400 p-2 bg-[#141414] outline-none"></textarea>
+                        <textarea value={sinopse}
+                        className="w-full h-[150px] rounded-lg border focus:border-pink-400 p-2 bg-[#141414] outline-none"></textarea>
                     </div>
                     <CustomInput
+                    onChange={
+                        (event) => setBanner(event.target.value)
+                    }
                     label="Banner"
+                    value={banner}
                     placeholder="URL da imagem"
                     type="text"
                     />
                     <div className="w-full flex justify-end gap-4">
                         <button type="reset" className="w-[100px] h-[50px] rounded-md bg-[#141414] border-[#3a364c] cursor-pointer hover:bg-white/10">Cancelar</button>
+                        <button type="submit" className="w-[130px] h-[50px] rounded-md bg-[#9A86F4] cursor-pointer font-bold hover:bg-[#9A86F4]/80">Salvar Filme</button>
 
                     </div>
 
@@ -71,5 +144,5 @@ export default function Cadastrar(){
                 </div>
             </div>
         </PageWrapper>
-    )
-}
+  )}
+
